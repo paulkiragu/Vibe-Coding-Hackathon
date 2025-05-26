ab3a55e8342910b38c7ce1bcf2c4498deae167e5
paul kiragu <paulkiragumburu@gmail.com>
2025-05-26 15:02:11 +0300

    fixed a bug of the form not submitting

--- scripts.js
+++ scripts.js
@@ -1,18 +1,20 @@
+const SUPABASE_URL = 'https://kluyhtskgcmufpkmdfsi.supabase.co';
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtsdXlodHNrZ2NtdWZwa21kZnNpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDgyNDkzODEsImV4cCI6MjA2MzgyNTM4MX0.wneKpK4qPvG5GjuZ6gacQbqVj7NyBpI_lJgUloC90hw';
 
-const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
 document.getElementById('appointmentForm').addEventListener('submit', async (e) => {
   e.preventDefault();
 
-  const name = document.getElementById('name').value;
-  const phone = document.getElementById('phone').value;
+  const name = document.getElementById('name').value.trim();
+  const phone = document.getElementById('phone').value.trim();
   const date = document.getElementById('date').value;
 
   const status = document.getElementById('status');
   status.textContent = '⏳ Submitting...';
   status.style.color = 'black';
 
-  const { data, error } = await client.from('patients').insert([
+  const { data, error } = await supabaseClient.from('patients').insert([
     { name, phone, appointment_date: date }
   ]);
 
